ad91c778d622a34a2d3b5bb379d18687670ad56b
Alejandro Perez Rubio <alejandro.perez3469@gmail.com>
2025-07-14 15:24:32 -0600

    Update text color for missing sections

--- components/templates/template1/SubdomainClientPage.tsx
+++ components/templates/template1/SubdomainClientPage.tsx
@@ -377,7 +377,7 @@ z"/>
                 </div>
             </div>
             <section className={`${styles.initialsSectionContainer}`}  style={{"backgroundColor" : subdomainData.MainColor}}>
-                <div className={`${styles.quoteText}`}>
+                <div className={`${styles.quoteText}`} style={{"color" : subdomainData.QuoteTextColor}}>
                     <div className={styles.subcontainer}>
                             {DressCodeIcon}
                         <p className={`${styles.normal} ${styles.title}`}>
@@ -418,7 +418,7 @@ z"/>
 
             <section className={`${styles.initialsSectionContainer}`}  style={{"backgroundColor" : subdomainData.MainColor}}>
 
-                <div className={`${styles.quoteText}`}>
+                <div className={`${styles.quoteText}`} style={{"color" : subdomainData.QuoteTextColor}}>
                     <div>
                         <svg className={styles.icon} data-slot="icon" fill="none" stroke-width="1.5" stroke="#63666A" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"></path>
@@ -453,7 +453,7 @@ z"/>
 
 
             <section className={`${styles.initialsSectionContainer}`}  style={{"backgroundColor" : subdomainData.MainColor}}>
-                <div className={`${styles.quoteText}`}>
+                <div className={`${styles.quoteText}`} style={{"color" : subdomainData.QuoteTextColor}}>
                     <p className={`${styles.normal} ${styles.title}`}>
                         {
                             "¿Contamos contigo?"
@@ -487,7 +487,7 @@ z"/>
             </section>
 
             <section className={`${styles.initialsSectionContainer}`}  style={{"backgroundColor" : subdomainData.MainColor}}>
-                <div className={`${styles.quoteText}`}>
+                <div className={`${styles.quoteText}`} style={{"color" : subdomainData.QuoteTextColor}}>
                     <p className={styles.accent}>
                         {
                             "¡Te esperamos!"
