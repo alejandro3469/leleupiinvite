6f6cf382b62af4bc0b6675c3f1a43894f11b15e5
alejandro.pr <alejandro.perez3469@gmail.com>
2025-07-19 22:46:29 -0600

    Update gifts section

--- components/templates/template1/SubdomainClientPage.tsx
+++ components/templates/template1/SubdomainClientPage.tsx
@@ -58,6 +58,8 @@ const CarouselDemo = () => {
                     </div>
                 </CarouselItem>
             </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
         </Carousel>
     )
 }
